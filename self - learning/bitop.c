d0fb4fdf029667209037cfebde0d07337273da69
limseungyeun <h92325926@gmail.com>
2026-04-01 15:46:47 +0900

    c-study-동아리 수업.c

---
+++ self - learning/bitop.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+	int a = 8;
+	printf("%d %d %d", a << 1, a >> 1, a & 3);
+	return 0;
+}
---
+++ self - learning/op3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int a = 10, b = 20;
+	int max;
+
+	max = (a > b) ? a : b;
+
+	return 0;
+}
